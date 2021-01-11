1aae32a6689513d3c839d277412fdb64bf0e4c50
mei-t <me.7tspr@gmail.com>
2021-01-11 09:57:51 +0900

    Fix sqlite3.ProgrammingError

--- TapSense/models/database.py
+++ TapSense/models/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tap.db')
-engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
+engine = create_engine('sqlite:///' + databese_file +'?check_same_thread=False', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
