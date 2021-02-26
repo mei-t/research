26148c4dc3268751af0ea24f820e9179194af51d
mei-t <me.7tspr@gmail.com>
2021-02-26 21:15:45 +0900

    Modify the path to tap.db

--- TapSense/models/database.py
+++ TapSense/models/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tap.db')
+databese_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tap.db')
+# databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tap.db')
 engine = create_engine('sqlite:///' + databese_file +'?check_same_thread=False', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
