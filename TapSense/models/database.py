ffe0a90497cdea14af64794b79263439239aba38
mei-t <me.7tspr@gmail.com>
2021-02-28 10:28:14 +0900

    Make read only database to writable

--- TapSense/models/database.py
+++ TapSense/models/database.py
@@ -3,10 +3,12 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-databese_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tap.db')
-# databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tap.db')
-engine = create_engine('sqlite:///' + databese_file +'?check_same_thread=False', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+database_path = '/tmp'
+if not os.path.exists(database_path):
+    os.makedirs(database_path)
+engine = create_engine('sqlite:///' + os.path.join(database_path, 'tap.db') +'?check_same_thread=False', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autoflush=False,bind=engine))
+# db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
